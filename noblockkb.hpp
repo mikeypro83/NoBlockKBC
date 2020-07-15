658f0b0bd08962664465fac756e2f5bad5fc9974
mikeypro83 <41976933+mikeypro83@users.noreply.github.com>
2020-07-14 19:38:58 -0600

    Add files via upload

--- noblockkb.hpp
+++ noblockkb.hpp
@@ -14,7 +14,35 @@
         original author: Michael Scott Stuart (mikeypro83)
         author email: (professional inquiries only) michaelscottstuart at yahoo dot com
     */
-   
+
+   /* 
+        LICENSE - (pretty much tells you you can do whatever you want with this source, 
+                    but nothing is my fault if it goes wrong or does not work)
+        
+        MIT License
+
+        Copyright (c) 2020 mikeypro83 (Michael Scott Stuart)
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+
+    */
+
     #ifdef __unix__
     #include "termios.h"
     #include <fcntl.h>
